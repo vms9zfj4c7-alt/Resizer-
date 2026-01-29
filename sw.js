91cd8eed742741450cd95a58584a1c2f55210d9d
vms9zfj4c7-alt <vms9zfj4c7@privaterelay.appleid.com>
2026-01-29 20:36:32 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -12,10 +12,10 @@ const ASSETS = [
   './icon-light.png',
   './manifest-dark.json',
   './manifest-light.json',
-  './assets/vendor/jszip.min.js',
-  './assets/vendor/FileSaver.min.js',
-  './assets/vendor/gif.js',
-  './assets/vendor/gif.worker.js'
+  'https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/FileSaver.js/2.0.5/FileSaver.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/gif.js/0.2.0/gif.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/gif.js/0.2.0/gif.worker.js'
 ];
 
 self.addEventListener('install', (event) => {
